6f04703a9a7bfbf5da89d261564555ad03cf1f6b
nicolaslazzos <nicolaslazzos09@gmail.com>
2019-05-21 12:50:49 -0300

    Conexion con Firebase

--- turnosya/src/App.js
+++ turnosya/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import firebase from 'firebase';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDBtphHkP2FAebuiBNkmGxLhxlPbHe10VI",
+      authDomain: "proyecto-turnosya.firebaseapp.com",
+      databaseURL: "https://proyecto-turnosya.firebaseio.com",
+      projectId: "proyecto-turnosya",
+      storageBucket: "proyecto-turnosya.appspot.com",
+      messagingSenderId: "425889819253",
+      appId: "1:425889819253:web:22821710c1e913a5"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>TurnosYa!</Text>
       </View>
     );
   }
