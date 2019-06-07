cde4f776cd4c265dbe8b93e2a2d756c0a1a18b9f
YunesAgustin <agustinyunes@gmail.com>
2019-06-07 20:12:36 -0300

    Login GUI

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,6 +4,7 @@ import { Card, Input, Button, Divider } from 'react-native-elements';
 import { View, Text } from 'react-native';
 import { CardSection } from './common';
 import { MAIN_COLOR } from '../constants';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class LoginForm extends Component {
   render() {
@@ -16,33 +17,96 @@ class LoginForm extends Component {
           padding: 15
         }}
       >
-        <CardSection>
-          <Input />
-        </CardSection>
-        <CardSection>
-          <Input />
-        </CardSection>
-        <CardSection>
-          <Button buttonStyle={styles.buttonStyle} title="Iniciar Sesion" />
-        </CardSection>
-        <Divider style={{ backgroundColor: 'grey' }} />
-        <CardSection>
-          <Button buttonStyle={styles.buttonStyle} title="Iniciar Sesion" />
-        </CardSection>
-        <CardSection>
-          <Button buttonStyle={styles.buttonStyle} title="Iniciar Sesion" />
-        </CardSection>
+        <View />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            // alignSelf: 'stretch',
+            marginBottom: 20
+          }}
+        >
+          <CardSection>
+            <Input
+              style={{ textAlign: 'center' }}
+              placeholder="Correo electrónico"
+            />
+          </CardSection>
+          <CardSection>
+            <Input placeholder="Contraseña" />
+          </CardSection>
+          <CardSection>
+            <Button
+              buttonStyle={styles.loginButtonStyle}
+              title="Iniciar Sesion"
+            />
+          </CardSection>
+          <Divider style={{ backgroundColor: 'grey', margin: 15 }} />
+          <CardSection>
+            <Button
+              buttonStyle={styles.googleButtonStyle}
+              title="Gmail"
+              icon={
+                <Icon
+                  name="google"
+                  size={22}
+                  color="#fff"
+                  style={{ marginRight: 10 }}
+                />
+              }
+            />
+            <Button
+              buttonStyle={styles.facebookButtonStyle}
+              title="Facebook"
+              icon={
+                <Icon
+                  name="facebook-square"
+                  size={22}
+                  color="#fff"
+                  style={{ marginRight: 10 }}
+                />
+              }
+            />
+          </CardSection>
+        </View>
+        <View style={{ justifyContent: 'flex-end' }}>
+          <CardSection>
+            <Button
+              buttonStyle={styles.createButtonStyle}
+              title="Crear cuenta"
+              type="outline"
+            />
+          </CardSection>
+        </View>
       </View>
     );
   }
 }
 
 const styles = {
-  buttonStyle: {
+  loginButtonStyle: {
     borderRadius: 10,
     padding: 10,
     margin: 10,
     backgroundColor: MAIN_COLOR
+  },
+  facebookButtonStyle: {
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#4267b2'
+  },
+  googleButtonStyle: {
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#de5145'
+  },
+  createButtonStyle: {
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    borderColor: '#fff'
   }
 };
 
