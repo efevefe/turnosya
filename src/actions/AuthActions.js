50c6921b3453ccd0913150322e2da42be84204fd
MCarballido <manu.carba96@gmail.com>
2019-06-15 15:15:25 -0300

    Login y Registro con Facebook

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -10,6 +10,28 @@ export const onLogin = ({ email, password }) => {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(user => dispatch({ type: ON_LOGIN_SUCCESS, payload: user }))
-      .catch(error => dispatch({ type: ON_LOGIN_FAIL, payload: error }));
+      .catch(error => dispatch({ type: ON_LOGIN_FAIL, payload: error.message }));
+  };
+};
+
+export const onLoginFacebook = () => {
+  return dispatch => {
+    dispatch({ type: ON_LOGIN });
+    Expo.Facebook.logInWithReadPermissionsAsync('308666633372616', {
+      permissions: ['public_profile']
+    })
+      .then(({ type, token }) => {
+        if (type === 'success') {
+          const credential = firebase.auth.FacebookAuthProvider.credential( token );
+          firebase
+            .auth()
+            .signInWithCredential(credential)
+            .then(user => dispatch({ type: ON_LOGIN_SUCCESS, payload: user }))
+            .catch(error => dispatch({ type: ON_LOGIN_FAIL, payload: error.message }));
+        } else {
+          dispatch({ type: ON_LOGIN_FAIL, payload: '' });
+        }
+      })
+      .catch(error => dispatch({ type: ON_LOGIN_FAIL, payload: error.message }));
   };
 };
