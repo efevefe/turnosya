3a8efb93bac6f128d9e63280f9ee09e6117371d9
ManuelMGM <manugm295@gmail.com>
2019-07-05 14:36:52 -0300

    intentando mejorar algo

--- src/components/CourtForm.js
+++ src/components/CourtForm.js
@@ -3,54 +3,68 @@ import firebase from 'firebase';
 import { connect } from 'react-redux';
 import { Card } from 'react-native-elements';
 import { View, StyleSheet } from 'react-native';
-import { CardSection, Button, Input, Picker } from './common';
+import { CardSection, Input, Picker } from './common';
 import { onCourtValueChange, onFormOpen } from '../actions';
 
 class CourtForm extends Component {
   constructor() {
     super();
-    this.state = { id: [], data: [] };
+    this.state = {
+      id: [],
+      data: [],
+      indexSelected: null,
+      typeSelected: null,
+      pickerEnable: false,
+      typeSelected2: null,
+      indexSelected2: null
+    };
   }
 
   componentDidMount() {
+    this.getDataBaseData();
+  }
+
+  getDataBaseData = () => {
     firebase
       .firestore()
       .collection('CourteType')
       .get()
       .then(querySnapshot => {
-        let id = [];
-        let data = [];
+        let id = ['Elija una opcion ...'];
+        let data = [{ '0': 'Elija una opcion ...' }];
         querySnapshot.forEach(doc => {
-          // doc.data() is never undefined for query doc snapshots
-          // console.log(doc.id, ' => ', doc.data());
           id.push(doc.id);
           data.push(doc.data());
         });
         this.setState({ id, data });
-        //console.log('ID: ', this.state.id);
-        console.log('DATA: ', this.state.data);
       });
-  }
+  };
 
   renderPicker = () => {
-    return (
-      <Picker>
-        {this.state.id.map((item, index) => {
+    if (this.state.id.length > 0) {
+      return this.state.id.map((item, index) => {
+        return <Picker.Item key={index} label={item} value={item} />;
+      });
+    } else {
+      return null;
+    }
+  };
+
+  renderPicker2 = () => {
+    if (this.state.indexSelected) {
+      return Object.values(this.state.data[this.state.indexSelected]).map(
+        (item, index) => {
           return <Picker.Item key={index} label={item} value={item} />;
-        })}
-      </Picker>
-    );
-    /*  <Picker>
-        <Picker.Item label={this.state.id[0]} value="js" />
-        <Picker.Item label={this.state.id[1]} value="js" />
-      </Picker> */
+        }
+      );
+    } else {
+      return null;
+    }
   };
 
   render() {
     return (
       <View>
-        {console.log('ID: ', this.state.id)}
-
         <Card containerStyle={styles.cardStyle}>
           <CardSection>
             <Input
@@ -65,20 +79,36 @@ class CourtForm extends Component {
               }
             />
           </CardSection>
+
           <CardSection>
-            <Input
-              label="Tipo de cancha:"
-              placeholder="Futbol"
-              value={this.props.typeCourt}
-              onChangeText={value =>
-                this.props.onCourtValueChange({
-                  prop: 'typeCourt',
-                  value
+            <Picker
+              selectedValue={this.state.typeSelected}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({
+                  indexSelected: itemIndex,
+                  typeSelected: itemValue,
+                  pickerEnable: true
                 })
               }
-            />
+            >
+              {this.renderPicker()}
+            </Picker>
           </CardSection>
-          <CardSection>{this.renderPicker()}</CardSection>
+
+          <CardSection>
+            <Picker
+              enable={this.state.pickerEnable}
+              selectedValue={this.state.typeSelected2}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({
+                  indexSelected2: itemIndex,
+                  typeSelected2: itemValue
+                })
+              }
+            >
+              {this.renderPicker2()}
+            </Picker>
+          </CardSection>
         </Card>
       </View>
     );
