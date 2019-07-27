7697a14a79c2c796b1d067d5a9d195ef7c40ab25
nicolaslazzos <nicolaslazzos09@gmail.com>
2019-07-26 22:14:48 -0300

    Cambio tamaño de letra del menu

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
     },
     titleStyle: {
         color: 'black',
-        fontSize: 18,
+        fontSize: 16,
         margin: 16,
         marginLeft: 18
     },
