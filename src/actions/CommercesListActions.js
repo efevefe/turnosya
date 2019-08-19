77318dd81e9a60531fa491e70fe54a697ddc0ad8
ManuelMGM <manugm295@gmail.com>
2019-08-19 13:40:12 -0300

    Mejorando la logica de busqueda de negocio

--- src/actions/CommercesListActions.js
+++ src/actions/CommercesListActions.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import {
-  ON_COMMERCES_READING, 
-  ON_COMMERCES_READ, 
-  ON_COMMERCES_SEARCHING, 
+  ON_COMMERCES_READING,
+  ON_COMMERCES_READ,
+  ON_COMMERCES_SEARCHING,
   ON_COMMERCES_SEARCHED
 } from './types';
 
@@ -31,12 +31,15 @@ export const searchCommerces = search => {
     dispatch({ type: ON_COMMERCES_SEARCHING });
     db.collection('Commerces')
       .where('softDelete', '==', null)
-      .orderBy('name', 'asc')
-      .startAt(search)
-      .endAt(`${search}\uf8ff`)
       .onSnapshot(snapShot => {
         var commerces = [];
-        snapShot.forEach(doc => commerces.push({ ...doc.data(), id: doc.id }));
+        snapShot.forEach(doc => {
+          const name = doc.data().name.toLowerCase();
+          if (name.includes(search.toLowerCase())) {
+            commerces.push({ ...doc.data(), id: doc.id });
+          }
+        });
+
         dispatch({ type: ON_COMMERCES_SEARCHED, payload: commerces });
       });
   };
