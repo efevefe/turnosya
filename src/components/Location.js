538f7cb7da97c18f0bce7978887ef37bfb73a080
YunesAgustin <agustinyunes@gmail.com>
2019-09-06 20:15:53 -0300

    Avanzes en la offi con manolo

--- src/components/Location.js
+++ src/components/Location.js
@@ -8,119 +8,57 @@ import {
   AlertIOS
 } from 'react-native';
 import { Permissions, Location, Constants, IntentLauncherAndroid } from 'expo';
+import {
+  getPermissionLocationStatus,
+  openGPSAndroid,
+  openSettingIos
+} from '../utils';
 
 export default class App extends Component {
   state = {
-    location: null,
-    errorMessage: null
+    location: null
   };
 
-  componentDidMount() {
-    this.getLocation();
+  async componentDidMount() {
+    this.setState({ location: await getPermissionLocationStatus() });
   }
 
-  getLocation = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-
-    if (status === 'denied') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied'
-      });
-      if (Platform.OS === 'ios') {
-        const GPSStatus = await Location.getProviderStatusAsync();
-        if (GPSStatus.locationServicesEnabled) {
-          this.setState({
-            errorMessage: 'Permisos APP'
-          });
-        } else {
-          this.setState({
-            errorMessage: 'GPSSS'
-          });
-          AlertIOS.alert(
-            'Activa la localización para tener una mejor experiencia',
-            'Dirigete a Ajustes > Privacidad > Localizacion',
-            [
-              {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel'
-              },
-              {
-                text: 'Configuracion',
-                onPress: () => Linking.openURL('App-Prefs:root=Privacy')
-                // onPress: () => Linking.openURL('app-settings:')
-              }
-            ]
-          );
-        }
+  renderLocation = () => {
+    const plat = Platform.OS;
+    if (plat === 'ios') {
+      switch (this.state.location) {
+        case '1':
+          return <Text>dame la posiotion</Text>;
+        case '2':
+          openSettingIos();
+          return <Text>los permisos no estas aceptados</Text>;
+        case '3':
+          return <Text>prende el gps</Text>;
+        default:
+          return;
       }
     } else {
-      if (Platform.OS === 'android') {
-        const GPSStatus = await Location.getProviderStatusAsync();
-        if (GPSStatus.gpsAvailable && GPSStatus.locationServicesEnabled) {
-          const location = await Location.getCurrentPositionAsync({
-            accuracy: Location.Accuracy.High
-          });
-
-          // location = Es un objeto. Tiene datos como:
-          //     coords  {
-          //        accuracy --> ,
-          //        altitude --> ,
-          //        heading --> ,
-          //        latitude --> ,
-          //        longitude --> ,
-          //        sped -->
-          //      }
-          //    mocked -->
-          //    timestamp -->
-
-          // let moreData = await Location.reverseGeocodeAsync({
-          //   latitude: location.coords.latitude,
-          //   longitude: location.coords.longitude
-          // });
-          // moreData = Es un array. Agrega datos como:
-          //     city --> Córdoba,
-          //     street --> null,
-          //     region --> Córdoba,
-          //     postalCode --> null,
-          //     country --> Argentina,
-          //     isoCountryCode --> AR,
-          //     name --> C1662
-          this.setState({ location });
-        } else {
-          // no te lo deberia abrir de una. Hay que ver como reacciona este metodo en ios
-          IntentLauncherAndroid.startActivityAsync(
-            IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS
-          ).then(async () => {
-            const GPSStatus = await Location.getProviderStatusAsync();
-            if (GPSStatus.gpsAvailable) {
-              const location = await Location.getCurrentPositionAsync({});
-              this.setState({ location });
-            } else {
-              this.setState({
-                errorMessage: 'Permission to access location was denied'
-              });
-            }
-          });
-        }
-      } else {
-        let location = await Location.getCurrentPositionAsync({});
-        this.setState({ location });
+      switch (this.state.location) {
+        case '1':
+          return <Text>dame la posiotion</Text>;
+        case '2':
+          return <Text>los permisos no estas aceptados</Text>;
+        case '3':
+          openGPSAndroid();
+          return <Text>prende el gps</Text>;
+        default:
+          return;
       }
     }
   };
 
   render() {
     let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
 
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>{text}</Text>
+        {this.renderLocation()}
       </View>
     );
   }
---
+++ src/utils/expoFunctions.js
@@ -0,0 +1,84 @@
+import { Platform, Linking, AlertIOS } from 'react-native';
+import { Permissions, Location, IntentLauncherAndroid } from 'expo';
+import { stringFormatDays } from './functions';
+
+const LocationStatus = {
+  permissionsAllowed: '1',
+  permissionsDenied: '2',
+  permissionsAllowedWithGPSOff: '3'
+};
+
+export const getPermissionLocationStatus = async () => {
+  const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+  return Platform.OS === 'ios'
+    ? getLocationIos(status)
+    : getLocationAndroid(status);
+};
+
+const getLocationAndroid = async status => {
+  if (status !== 'denied') {
+    return (await Location.hasServicesEnabledAsync())
+      ? LocationStatus.permissionsAllowed
+      : LocationStatus.permissionsAllowedWithGPSOff;
+  }
+
+  return LocationStatus.permissionsDenied;
+};
+
+const getLocationIos = async status => {
+  if (status === 'denied') {
+    return (await Location.hasServicesEnabledAsync())
+      ? LocationStatus.permissionsDenied
+      : LocationStatus.permissionsAllowedWithGPSOff;
+  }
+
+  return LocationStatus.permissionsAllowed;
+};
+
+export const getCurrentPosition = async () => {
+  return await Location.getCurrentPositionAsync({
+    accuracy: Location.Accuracy.High
+  });
+};
+
+export const openGPSAndroid = () => {
+  IntentLauncherAndroid.startActivityAsync(
+    IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS
+  ).then(async () => {
+    if (await Location.hasServicesEnabledAsync()) {
+      return LocationStatus.permissionsAllowed;
+    }
+
+    return LocationStatus.permissionsAllowedWithGPSOff;
+  });
+};
+
+export const openSettingIos = () => {
+  Linking.openURL('app-settings:');
+};
+
+// location = Es un objeto. Tiene datos como:
+//     coords  {
+//        accuracy --> ,
+//        altitude --> ,
+//        heading --> ,
+//        latitude --> ,
+//        longitude --> ,
+//        sped -->
+//      }
+//    mocked -->
+//    timestamp -->
+
+// let moreData = await Location.reverseGeocodeAsync({
+//   latitude: location.coords.latitude,
+//   longitude: location.coords.longitude
+// });
+// moreData = Es un array. Agrega datos como:
+//     city --> Córdoba,
+//     street --> null,
+//     region --> Córdoba,
+//     postalCode --> null,
+//     country --> Argentina,
+//     isoCountryCode --> AR,
+//     name --> C1662
