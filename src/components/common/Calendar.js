3a60295d9ce766be1461c46bc9159d1dac42d898
nicolaslazzos <nicolaslazzos09@gmail.com>
2019-09-09 18:54:34 -0300

    Limitacion al resizing del calendar

--- src/components/common/Calendar.js
+++ src/components/common/Calendar.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
 import CalendarStrip from 'react-native-calendar-strip';
 import { Ionicons } from '@expo/vector-icons';
 import { MAIN_COLOR, MAIN_COLOR_DISABLED } from '../../constants';
+
+//const height = Math.round((Dimensions.get('window').width / Dimensions.get('window').height) * 230);
 
 class Calendar extends Component {
   render() {
@@ -16,14 +19,15 @@ class Calendar extends Component {
         }}
         calendarColor={MAIN_COLOR}
         style={{ height: 115, paddingTop: 10, paddingBottom: 10 }} //main container
-        calendarHeaderStyle={{ color: 'white' }} //no se bien
-        calendarHeaderContainerStyle={{ paddingBottom: 10 }} //mes y año
+        calendarHeaderContainerStyle={{ paddingBottom: 10 }} // mes y año container
+        calendarHeaderStyle={{ color: 'white' }} // mes y año text
         dateNameStyle={{ color: 'white' }}
         dateNumberStyle={{ color: 'white' }}
-        highlightDateNumberStyle={{ color: MAIN_COLOR }}
         highlightDateNameStyle={{ color: MAIN_COLOR }}
+        highlightDateNumberStyle={{ color: MAIN_COLOR }}
         disabledDateNameStyle={{ color: MAIN_COLOR_DISABLED }}
         disabledDateNumberStyle={{ color: MAIN_COLOR_DISABLED }}
+        maxDayComponentSize={60}
         iconContainer={{ flex: 0.1 }}
         leftSelector={
           <Ionicons name="ios-arrow-back" color="white" size={25} />
