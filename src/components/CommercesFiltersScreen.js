7e1c6ed224e41289b22296813b431092483c6f2f
MCarballido <manu.carba96@gmail.com>
2019-10-12 17:36:08 -0300

    Bosquejo de Interfaz de usuario del filtro de localización

--- src/components/CommercesFiltersScreen.js
+++ src/components/CommercesFiltersScreen.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Divider, Button } from 'react-native-elements';
+import { View, Text, StyleSheet } from 'react-native';
+import { Divider, Button, ButtonGroup, Slider } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { IconButton, Picker } from './common';
-import { MAIN_COLOR } from '../constants';
+import { MAIN_COLOR, MAIN_COLOR_OPACITY } from '../constants';
 import { onProvincesNameRead, updateProvinceFilter } from '../actions';
 
 class CommerceFiltersScreen extends Component {
@@ -30,53 +30,27 @@ class CommerceFiltersScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: MAIN_COLOR }}>
-        <View
-          style={{
-            paddingTop: 20,
-            height: 70,
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: 'row'
-          }}
-        >
+      <View style={windowContainerStyle}>
+        <View style={windowTopContainerStyle}>
           <IconButton icon="md-close" onPress={this.onClosePress} />
           <Button
             title="Aplicar Filtros"
             type="clear"
             titleStyle={{ color: 'white' }}
             onPress={this.onApplyFiltersPress.bind(this)}
-            style={{ marginRight: 10, padding: 5 }}
+            style={applyFilterButtonStyle}
           />
         </View>
-        <View style={{ flex: 1, alignItems: 'center' }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-            <Divider
-              style={{
-                backgroundColor: 'white',
-                flex: 1,
-                alignSelf: 'center',
-                marginHorizontal: 5
-              }}
-            />
-            <Text style={{ color: 'white', padding: 5 }}>Provincia</Text>
-            <Divider
-              style={{
-                backgroundColor: 'white',
-                flex: 1,
-                alignSelf: 'center',
-                marginHorizontal: 5
-              }}
-            />
+        <View style={windowContentContainerStyle}>
+          {/* Divisor */}
+          <View style={dividerContainerStyle}>
+            <Divider style={dividerStyle} />
+            <Text style={dividerTextStyle}>Provincia</Text>
+            <Divider style={dividerStyle} />
           </View>
+          {/* Divisor */}
 
-          <View
-            style={{
-              alignSelf: 'stretch',
-              paddingBottom: 20,
-              paddingHorizontal: 10
-            }}
-          >
+          <View style={provinceContainerStyle}>
             <Picker
               placeholder={{ value: '', label: 'Todas' }}
               value={this.state.provinceName}
@@ -86,11 +60,101 @@ class CommerceFiltersScreen extends Component {
               textColor="white"
             />
           </View>
+
+          {/* Divisor */}
+          <View style={dividerContainerStyle}>
+            <Divider style={dividerStyle} />
+            <Text style={dividerTextStyle}>Localización</Text>
+            <Divider style={dividerStyle} />
+          </View>
+          {/* Divisor */}
+
+          <View style={locationContainerStyle}>
+            <ButtonGroup
+              // onPress={this.onPriceSelect}
+              selectedIndex={0}
+              buttons={[
+                'Deshabilitada',
+                'Localización actual',
+                'Ubicación en mapa'
+              ]}
+              selectedButtonStyle={{ backgroundColor: MAIN_COLOR }}
+              selectedTextStyle={{ color: 'white' }}
+              textStyle={{ color: MAIN_COLOR, textAlign: 'center' }}
+              containerStyle={{
+                borderColor: 'white',
+                height: 45,
+                marginTop: 15,
+                borderRadius: 8
+              }}
+              innerBorderStyle={{ color: MAIN_COLOR }}
+            />
+            <Text style={locationTextStyle}>Radio de búsqueda: 100 km</Text>
+            <Slider
+              style={locationSlideStyle}
+              animationType="spring"
+              minimumTrackTintColor={MAIN_COLOR_OPACITY}
+              // minimumValue={reservationMinFrom}
+              // maximumValue={reservationMinTo}
+              // step={reservationMinFrom}
+              thumbTouchSize={{ width: 60, height: 60 }}
+              thumbTintColor="white"
+              // value={reservationMinValue}
+              // onSlidingComplete={this.onMinSliderValueChange.bind(this)}
+              // onValueChange={val => this.setState({ reservationMinValue: val })}
+            />
+          </View>
         </View>
       </View>
     );
   }
 }
+
+const {
+  dividerStyle,
+  dividerTextStyle,
+  dividerContainerStyle,
+  windowContainerStyle,
+  windowTopContainerStyle,
+  windowContentContainerStyle,
+  applyFilterButtonStyle,
+  provinceContainerStyle,
+  locationContainerStyle,
+  locationTextStyle,
+  locationSlideStyle
+} = StyleSheet.create({
+  dividerStyle: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignSelf: 'center',
+    marginHorizontal: 5
+  },
+  dividerTextStyle: { color: 'white', padding: 5 },
+  dividerContainerStyle: { flexDirection: 'row', justifyContent: 'center' },
+  windowContainerStyle: { flex: 1, backgroundColor: MAIN_COLOR },
+  windowTopContainerStyle: {
+    paddingTop: 20,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  windowContentContainerStyle: { flex: 1, alignItems: 'center' },
+  applyFilterButtonStyle: { marginRight: 10, padding: 5 },
+  provinceContainerStyle: {
+    alignSelf: 'stretch',
+    paddingBottom: 20,
+    paddingHorizontal: 10
+  },
+  locationContainerStyle: { padding: 5, alignSelf: 'stretch', flex: 1 },
+  locationTextStyle: {
+    color: 'white',
+    marginTop: 15,
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  locationSlideStyle: { marginHorizontal: 15 }
+});
 
 const mapStateToProps = state => {
   const { provincesList } = state.provinceData;
