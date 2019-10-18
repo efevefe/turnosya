f901b8504e51f5b0ba9fc4f09aa13a5268db0241
nicolaslazzos <nicolaslazzos09@gmail.com>
2019-10-18 15:51:01 -0300

    Cambio la navegacion del login

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
 import { Root } from 'native-base';
 import MainNavigation from './src/navigation/MainNavigation';
-import GuestNavigation from './src/navigation/GuestNavigation';
 import reducers from './src/reducers';
-import LoadingScreen from './src/components/LoadingScreen';
 import { MAIN_COLOR } from './src/constants';
 
 import getEnvVars from './environment';
@@ -23,30 +21,12 @@ console.disableYellowBox = true;
 const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 class App extends React.Component {
-  state = { logged: false, screenLoading: true };
-
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      user
-        ? this.setState({ logged: true, screenLoading: false })
-        : this.setState({ logged: false, screenLoading: false });
-    });
-  }
-
-  renderNavigation = () => {
-    const { screenLoading, logged } = this.state;
-
-    if (screenLoading) return <LoadingScreen />;
-
-    return logged ? <MainNavigation /> : <GuestNavigation />;
-  };
-
   render() {
     return (
       <Provider store={store}>
         <Root>
           <StatusBar backgroundColor={MAIN_COLOR} barStyle="light-content" />
-          <View style={styles.container}>{this.renderNavigation()}</View>
+          <View style={styles.container}><MainNavigation /></View>
         </Root>
       </Provider>
     );
--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,14 +1,18 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import ClientDrawer from './ClientDrawer';
 import CommerceDrawer from './CommerceDrawer';
+import GuestNavigation from './GuestNavigation';
+import LoadingScreen from '../components/LoadingScreen';
 
 const mainNavigation = createSwitchNavigator(
   {
+    loading: LoadingScreen,
+    login: GuestNavigation,
     client: ClientDrawer,
     commerce: CommerceDrawer
   },
   {
-    initialRouteName: 'client'
+    initialRouteName: 'loading'
   }
 );
 
