369bc746b80c2ce00526f537f8668d75a89c397e
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-01-06 04:40:26 -0300

    Otras mejoras de styling

--- src/components/commerce/reports/DashBoard.js
+++ src/components/commerce/reports/DashBoard.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withNavigationFocus } from 'react-navigation';
-import { Text, TouchableHighlight, ScrollView } from 'react-native';
+import { Text, TouchableHighlight, ScrollView, StyleSheet } from 'react-native';
 import { Card } from 'react-native-elements';
 import { onCommerceReportValueReset } from '../../../actions';
 
-const imageRoute = '../../../../assets/'
+const imagesRoute = '../../../../assets/';
 
 const chartsData = [
   {
     id: 1,
     title: 'Cantidad de reservas por día',
-    image: require(imageRoute + 'bar-chart.png'),
+    image: require(imagesRoute + 'bar-chart.png'),
     screen: 'barChartReport',
     description: 'Visualize la cantidad de reservas que tiene su negocio por dia. ' +
       'Esto lo ayuda a saber la demanda de cada dia y en base a eso poder ' +
@@ -20,7 +19,7 @@ const chartsData = [
   {
     id: 2,
     title: 'Ingresos por mes',
-    image: require(imageRoute + 'earnings-chart.png'),
+    image: require(imagesRoute + 'earnings-chart.png'),
     screen: 'lineChartMoneyReport',
     description: 'Muestra la cantidad de ingresos percibidos en cada mes de un año. ' +
       'De esta manera puede visualizar la diferencia de ingresos en los ' +
@@ -29,7 +28,7 @@ const chartsData = [
   {
     id: 3,
     title: 'Mis calificaciones',
-    image: require(imageRoute + 'ratings-chart.png'),
+    image: require(imagesRoute + 'ratings-chart.png'),
     screen: 'lineChartReviewsReport',
     description: 'Haga un seguimiento de las calificaciones de sus clientes. Nos ' +
       'ayuda a observar las fluctuaciones de las calificaciones a lo ' +
@@ -38,22 +37,22 @@ const chartsData = [
   {
     id: 4,
     title: 'Cantidad de turnos cancelados y realizados',
-    image: require(imageRoute + 'reservations-chart.png'),
+    image: require(imagesRoute + 'reservations-chart.png'),
     screen: 'pieChartReport',
     description: 'Haga una comparación entre la cantidad de reservas realizadas y ' +
       'las canceladas.'
   }
-]
+];
 
 class DashBoard extends Component {
   componentDidUpdate(prevProps) {
-    if (this.props.isFocused && !prevProps.isFocused)
+    if (this.props.isFocused() && !prevProps.isFocused())
       this.props.onCommerceReportValueReset();
   }
 
   render() {
     return (
-      <ScrollView contentContainerStyle={{ paddingBottom: 15 }}>
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
         {
           chartsData.map(chart => (
             <TouchableHighlight
@@ -63,25 +62,13 @@ class DashBoard extends Component {
             >
               <Card
                 image={chart.image}
-                containerStyle={{ borderRadius: 10, overflow: 'hidden' }}
+                containerStyle={styles.cardContainer}
                 imageProps={{ resizeMode: 'stretch' }}
               >
-                <Text
-                  style={{
-                    fontSize: 16,
-                    textAlign: 'center'
-                  }}
-                >
+                <Text style={styles.cardTitle}>
                   {chart.title}
                 </Text>
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontWeight: '300',
-                    textAlign: 'center',
-                    marginTop: 5
-                  }}
-                >
+                <Text style={styles.cardDescription}>
                   {chart.description}
                 </Text>
               </Card>
@@ -93,6 +80,24 @@ class DashBoard extends Component {
   }
 }
 
-export default connect(null, { onCommerceReportValueReset })(
-  withNavigationFocus(DashBoard)
-);
+const styles = StyleSheet.create({
+  scrollViewContainer: {
+    paddingBottom: 15
+  },
+  cardContainer: {
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  cardTitle: {
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  cardDescription: {
+    fontSize: 12,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 5
+  }
+});
+
+export default connect(null, { onCommerceReportValueReset })(DashBoard);
