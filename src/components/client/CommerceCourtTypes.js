0e26cd4432039d914a624440a171d2693a3b024d
Manuel Garcia Masjoan <manugm295@gmail.com>
2020-01-18 12:33:35 -0300

    se verifica que el mail este validado para reservar turnos

--- src/components/client/CommerceCourtTypes.js
+++ src/components/client/CommerceCourtTypes.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
-import {
-  FlatList,
-  Text,
-  TouchableHighlight
-} from 'react-native';
+import { FlatList, Text, TouchableHighlight, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { EmptyList } from '../common';
-import {
-  onCourtReservationValueChange
-} from '../../actions';
+import { onCourtReservationValueChange } from '../../actions';
+import VerifyEmailModal from '../client/VerifyEmailModal';
+import { isEmailVerified } from '../../utils';
 
 class CommerceCourtTypes extends Component {
-  onCourtTypePress = courtType => {
-    this.props.onCourtReservationValueChange({
-      prop: 'courtType',
-      value: courtType
-    });
+  state = { modal: false };
 
-    this.props.navigation.navigate('commerceSchedule');
+  onCourtTypePress = async courtType => {
+    try {
+      if (await isEmailVerified()) {
+        this.props.onCourtReservationValueChange({ courtType });
+        this.props.navigation.navigate('commerceSchedule');
+      } else {
+        this.setState({ modal: true });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  onModalClose = () => {
+    this.setState({ modal: false });
+  };
+
+  renderModal = () => {
+    if (this.state.modal)
+      return <VerifyEmailModal onModalCloseCallback={this.onModalClose} />;
   };
 
   renderItem = ({ item }) => {
     return (
       <TouchableHighlight
-        onPress={this.props.navigation.state.routeName === 'commerceProfileView'
-          ? (() => this.onCourtTypePress(item.name)) : null}
+        onPress={
+          this.props.navigation.state.routeName === 'commerceProfileView'
+            ? () => this.onCourtTypePress(item.name)
+            : null
+        }
         underlayColor="transparent"
       >
         <Card
@@ -47,21 +61,20 @@ class CommerceCourtTypes extends Component {
 
     if (courtTypesList.length) {
       return (
-        <FlatList
-          data={courtTypesList}
-          renderItem={this.renderItem}
-          keyExtractor={courtType => courtType.name}
-          contentContainerStyle={{ paddingBottom: 15 }}
-        />
+        <View>
+          <FlatList
+            data={courtTypesList}
+            renderItem={this.renderItem}
+            keyExtractor={courtType => courtType.name}
+            contentContainerStyle={{ paddingBottom: 15 }}
+          />
+          {this.renderModal()}
+        </View>
       );
     }
 
-    return (
-      <EmptyList
-        title="Parece que no hay canchas"
-      />
-    );
-  };
+    return <EmptyList title="Parece que no hay canchas" />;
+  }
 }
 
 const mapStateToProps = state => {
@@ -69,9 +82,6 @@ const mapStateToProps = state => {
   return { courtTypesList };
 };
 
-export default connect(
-  mapStateToProps,
-  {
-    onCourtReservationValueChange
-  }
-)(CommerceCourtTypes);
+export default connect(mapStateToProps, {
+  onCourtReservationValueChange
+})(CommerceCourtTypes);
